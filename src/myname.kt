e4a694c1f6b5275e393502701edd07c222786851
maryakinyi <owuorakinyimary@gmail.com>
2021-03-17 18:34:38 +0300

    first commit

---
+++ src/myname.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val name = "mary"
+    println(name)
+    var text="Mary Akinyi"
+    var text2="EDSTY9SDKI2 confirmed , you have received 200 shillingsfrom walter"
+    var state=("Mary Akinyi .EDSTY9SDKI2 confirmed,you have receive 200 shillings")
+    println(state)
+    val word=("fish,sheep,gaot,dog,vulture")
+    val animal= word.split("fish,sheep,goat,dog,vulture")
+    println(animal)
+
+
+}
+fun vest(text:String,text2:String){
+    var state="Mary Akinyi" + "EDSTY9SDKI2 confirmed .You have receive 200 shillings from walter"
+    println(state)
+}
+fun crawl(word:String){
+    val word="fish,sheep,goat,dog,vulture"
+    println()
+}
